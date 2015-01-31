8ea76ad898f2e364dd22d6e6f60e8c4ccbcb93cd
Swapnil-Prazas <swapnil@prazas.com>
2015-01-31 13:04:01 +0530

    first commit

---
+++ Stayzilla/Stayzilla/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stayzilla
+//
+//  Created by Swapnil Patil on 31/01/15.
+//  Copyright (c) 2015 Blackhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stayzilla/Stayzilla/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Stayzilla
+//
+//  Created by Swapnil Patil on 31/01/15.
+//  Copyright (c) 2015 Blackhat. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
